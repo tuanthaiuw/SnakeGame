4985e0dc2afb8ad23fdd435c0047cbb7b9850c06
tuanthaiuw <50390186+tuanthaiuw@users.noreply.github.com>
2019-05-07 23:46:44 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,7 @@ let height = canvas.height;
 addToSnake(0, 0);
 addToSnake(10, 0);
 addToSnake(20, 0);
-setInterval(update, 75);
+setInterval(update, 25);
 addFood();
 
 window.addEventListener("keydown", keys);
